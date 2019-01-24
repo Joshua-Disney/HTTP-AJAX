d536e3150b93e1232bcc64980e93cb2d575a7e9c
joshua-disney <joshua.a.disney@gmail.com>
2019-01-23 18:33:55 -0700

    Rendered json data to the screen.

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Friend extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.friend.name}
+        {this.props.friend.age}
+        {this.props.friend.email}
+      </div>
+    )
+  }
+}
+
+export default Friend;
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 
+import Friend from './Friend';
+
 class FriendsList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: []
+      friends: [],
+      error: ''
     }
   }
 
   componentDidMount() {
-    axios.get('http://localhost:3000/friends')
+    axios.get('http://localhost:5000/friends')
     .then(response => {
-      console.log(response.status);
+      this.setState({friends: response.data});
     })
     .catch(error => {
       console.log(error);
@@ -20,9 +23,13 @@ class FriendsList extends React.Component {
   }
 
   render() {
+    console.log(this.state.friends);
     return(
       <div>
         <h1>Is this working?</h1>
+        {this.state.friends.map(friend => (
+          <Friend friend={friend} />
+        ))}
       </div>
     )
   }
